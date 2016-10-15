d08b5efcd4c1fac24908afea0d35472848cf59a8
BlueBoxWare <BlueBoxWare@users.noreply.github.com>
2016-10-15 18:55:27 +0200

    Create FlatUI.java

---
+++ FlatUI.java
@@ -0,0 +1,44 @@
+import com.badlogic.gdx.graphics.Color;
+
+/*
+ *
+ * Flat UI Colors
+ *
+ * http://designmodo.github.io/Flat-UI/
+ *
+ */
+@SuppressWarnings("unused, WeakerAccess")
+
+public class FlatUI {
+
+    public static final Color turquoise    = new Color(0x1ABC9CFF);
+    public static final Color green_sea    = new Color(0x16A085FF);
+
+    public static final Color emerald      = new Color(0x2ECC71FF);
+    public static final Color nephritis    = new Color(0x27AE60FF);
+
+    public static final Color peter_river  = new Color(0x3498DBFF);
+    public static final Color belize_hole  = new Color(0x2980B9FF);
+
+    public static final Color amethyst     = new Color(0x9B59B6FF);
+    public static final Color wisteria     = new Color(0x8E44ADFF);
+
+    public static final Color wet_asphalt  = new Color(0x34495EFF);
+    public static final Color midnight_blue = new Color(0x2C3E50FF);
+
+    public static final Color sunflower    = new Color(0xF1C40FFF);
+    public static final Color orange       = new Color(0xF39C12FF);
+
+    public static final Color carrot       = new Color(0xE67E22FF);
+    public static final Color pumpkin      = new Color(0xD35400FF);
+
+    public static final Color alizarin     = new Color(0xE74C3CFF);
+    public static final Color pomegranate  = new Color(0xC0392BFF);
+
+    public static final Color clouds       = new Color(0xECF0F1FF);
+    public static final Color silver       = new Color(0xBDC3C7FF);
+
+    public static final Color concrete     = new Color(0x95A5A6FF);
+    public static final Color asbestos     = new Color(0x7F8C8DFF);
+    
+}
