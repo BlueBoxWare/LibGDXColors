38af7a386273b1773783846e13ad5fb05f981e27
BlueBoxWare <BlueBoxWare@users.noreply.github.com>
2016-10-15 18:54:57 +0200

    Create FlatUI.kt

---
+++ FlatUI.kt
@@ -0,0 +1,44 @@
+import com.badlogic.gdx.graphics.Color
+
+/*
+ *
+ * Flat UI Colors
+ *
+ * http://designmodo.github.io/Flat-UI/
+ *
+ */
+
+@Suppress("unused")
+object FlatUI {
+
+    val turquoise: Color    = Color.valueOf("#1ABC9C")
+    val green_sea: Color    = Color.valueOf("#16A085")
+    
+    val emerald: Color      = Color.valueOf("#2ECC71")
+    val nephritis: Color    = Color.valueOf("#27AE60")
+    
+    val peter_river: Color  = Color.valueOf("#3498DB")
+    val belize_hole: Color  = Color.valueOf("#2980B9")
+    
+    val amethyst: Color     = Color.valueOf("#9B59B6")
+    val wisteria: Color     = Color.valueOf("#8E44AD")
+    
+    val wet_asphalt: Color  = Color.valueOf("#34495E")
+    val midnight_blue: Color = Color.valueOf("#2C3E50")
+    
+    val sunflower: Color    = Color.valueOf("#F1C40F")
+    val orange: Color       = Color.valueOf("#F39C12")
+    
+    val carrot: Color       = Color.valueOf("#E67E22")
+    val pumpkin: Color      = Color.valueOf("#D35400")
+    
+    val alizarin: Color     = Color.valueOf("#E74C3C")
+    val pomegranate: Color  = Color.valueOf("#C0392B")
+    
+    val clouds: Color       = Color.valueOf("#ECF0F1")
+    val silver: Color       = Color.valueOf("#BDC3C7")
+    
+    val concrete: Color     = Color.valueOf("#95A5A6")
+    val asbestos: Color     = Color.valueOf("#7F8C8D")
+
+}
