63361d193c363fe6ac15a7d1d601b9a2002f8682
BlueBoxWare <BlueBoxWare@users.noreply.github.com>
2016-10-15 18:53:09 +0200

    Create Material.kt

---
+++ Material.kt
@@ -0,0 +1,447 @@
+import com.badlogic.gdx.graphics.Color
+
+/*
+ *
+ * Material Design Colors
+ *
+ * https://material.google.com/style/color.html
+ *
+ */
+@Suppress("unused")
+object Material {
+
+    /** reds */
+    val red_50: Color = Color.valueOf("#FFEBEE")
+    val red_100: Color = Color.valueOf("#FFCDD2")
+    val red_200: Color = Color.valueOf("#EF9A9A")
+    val red_300: Color = Color.valueOf("#E57373")
+    val red_400: Color = Color.valueOf("#EF5350")
+    val red_500: Color = Color.valueOf("#F44336")
+    val red_600: Color = Color.valueOf("#E53935")
+    val red_700: Color = Color.valueOf("#D32F2F")
+    val red_800: Color = Color.valueOf("#C62828")
+    val red_900: Color = Color.valueOf("#B71C1C")
+    val red_A100: Color = Color.valueOf("#FF8A80")
+    val red_A200: Color = Color.valueOf("#FF5252")
+    val red_A400: Color = Color.valueOf("#FF1744")
+    val red_A700: Color = Color.valueOf("#D50000")
+
+    val red_primary = red_500
+    val red_primary_light = red_100
+    val red_primary_dark = red_700
+
+    val red_accent = red_A200
+    val red_accent_light = red_A100
+    val red_accent_dark = red_A400
+
+    /** pinks */
+    val pink_50: Color = Color.valueOf("#FCE4EC")
+    val pink_100: Color = Color.valueOf("#F8BBD0")
+    val pink_200: Color = Color.valueOf("#F48FB1")
+    val pink_300: Color = Color.valueOf("#F06292")
+    val pink_400: Color = Color.valueOf("#EC407A")
+    val pink_500: Color = Color.valueOf("#E91E63")
+    val pink_600: Color = Color.valueOf("#D81B60")
+    val pink_700: Color = Color.valueOf("#C2185B")
+    val pink_800: Color = Color.valueOf("#AD1457")
+    val pink_900: Color = Color.valueOf("#880E4F")
+    val pink_A100: Color = Color.valueOf("#FF80AB")
+    val pink_A200: Color = Color.valueOf("#FF4081")
+    val pink_A400: Color = Color.valueOf("#F50057")
+    val pink_A700: Color = Color.valueOf("#C51162")
+
+    val pink_primary = pink_500
+    val pink_primary_light = pink_100
+    val pink_primary_dark = pink_700
+
+    val pink_accent = pink_A200
+    val pink_accent_light = pink_A100
+    val pink_accent_dark = pink_A400
+
+    /** purples */
+    val purple_50: Color = Color.valueOf("#F3E5F5")
+    val purple_100: Color = Color.valueOf("#E1BEE7")
+    val purple_200: Color = Color.valueOf("#CE93D8")
+    val purple_300: Color = Color.valueOf("#BA68C8")
+    val purple_400: Color = Color.valueOf("#AB47BC")
+    val purple_500: Color = Color.valueOf("#9C27B0")
+    val purple_600: Color = Color.valueOf("#8E24AA")
+    val purple_700: Color = Color.valueOf("#7B1FA2")
+    val purple_800: Color = Color.valueOf("#6A1B9A")
+    val purple_900: Color = Color.valueOf("#4A148C")
+    val purple_A100: Color = Color.valueOf("#EA80FC")
+    val purple_A200: Color = Color.valueOf("#E040FB")
+    val purple_A400: Color = Color.valueOf("#D500F9")
+    val purple_A700: Color = Color.valueOf("#AA00FF")
+
+    val purple_primary = purple_500
+    val purple_primary_light = purple_100
+    val purple_primary_dark = purple_700
+
+    val purple_accent = purple_A200
+    val purple_accent_light = purple_A100
+    val purple_accent_dark = purple_A400
+
+    /** deep purples */
+    val deep_purple_50: Color = Color.valueOf("#EDE7F6")
+    val deep_purple_100: Color = Color.valueOf("#D1C4E9")
+    val deep_purple_200: Color = Color.valueOf("#B39DDB")
+    val deep_purple_300: Color = Color.valueOf("#9575CD")
+    val deep_purple_400: Color = Color.valueOf("#7E57C2")
+    val deep_purple_500: Color = Color.valueOf("#673AB7")
+    val deep_purple_600: Color = Color.valueOf("#5E35B1")
+    val deep_purple_700: Color = Color.valueOf("#512DA8")
+    val deep_purple_800: Color = Color.valueOf("#4527A0")
+    val deep_purple_900: Color = Color.valueOf("#311B92")
+    val deep_purple_A100: Color = Color.valueOf("#B388FF")
+    val deep_purple_A200: Color = Color.valueOf("#7C4DFF")
+    val deep_purple_A400: Color = Color.valueOf("#651FFF")
+    val deep_purple_A700: Color = Color.valueOf("#6200EA")
+
+    val deep_purple_primary = deep_purple_500
+    val deep_purple_primary_light = deep_purple_100
+    val deep_purple_primary_dark = deep_purple_700
+
+    val deep_purple_accent = deep_purple_A200
+    val deep_purple_accent_light = deep_purple_A100
+    val deep_purple_accent_dark = deep_purple_A400
+
+    /** indigo */
+    val indigo_50: Color = Color.valueOf("#E8EAF6")
+    val indigo_100: Color = Color.valueOf("#C5CAE9")
+    val indigo_200: Color = Color.valueOf("#9FA8DA")
+    val indigo_300: Color = Color.valueOf("#7986CB")
+    val indigo_400: Color = Color.valueOf("#5C6BC0")
+    val indigo_500: Color = Color.valueOf("#3F51B5")
+    val indigo_600: Color = Color.valueOf("#3949AB")
+    val indigo_700: Color = Color.valueOf("#303F9F")
+    val indigo_800: Color = Color.valueOf("#283593")
+    val indigo_900: Color = Color.valueOf("#1A237E")
+    val indigo_A100: Color = Color.valueOf("#8C9EFF")
+    val indigo_A200: Color = Color.valueOf("#536DFE")
+    val indigo_A400: Color = Color.valueOf("#3D5AFE")
+    val indigo_A700: Color = Color.valueOf("#304FFE")
+
+    val indigo_primary = indigo_500
+    val indigo_primary_light = indigo_100
+    val indigo_primary_dark = indigo_700
+
+    val indigo_accent = indigo_A200
+    val indigo_accent_light = indigo_A100
+    val indigo_accent_dark = indigo_A400
+
+    /**blue*/
+    val blue_50: Color = Color.valueOf("#E3F2FD")
+    val blue_100: Color = Color.valueOf("#BBDEFB")
+    val blue_200: Color = Color.valueOf("#90CAF9")
+    val blue_300: Color = Color.valueOf("#64B5F6")
+    val blue_400: Color = Color.valueOf("#42A5F5")
+    val blue_500: Color = Color.valueOf("#2196F3")
+    val blue_600: Color = Color.valueOf("#1E88E5")
+    val blue_700: Color = Color.valueOf("#1976D2")
+    val blue_800: Color = Color.valueOf("#1565C0")
+    val blue_900: Color = Color.valueOf("#0D47A1")
+    val blue_A100: Color = Color.valueOf("#82B1FF")
+    val blue_A200: Color = Color.valueOf("#448AFF")
+    val blue_A400: Color = Color.valueOf("#2979FF")
+    val blue_A700: Color = Color.valueOf("#2962FF")
+
+    val blue_primary = blue_500
+    val blue_primary_light = blue_100
+    val blue_primary_dark = blue_700
+
+    val blue_accent = blue_A200
+    val blue_accent_light = blue_A100
+    val blue_accent_dark = blue_A400
+
+    /** light blue*/
+    val light_blue_50: Color = Color.valueOf("#E1F5FE")
+    val light_blue_100: Color = Color.valueOf("#B3E5FC")
+    val light_blue_200: Color = Color.valueOf("#81D4fA")
+    val light_blue_300: Color = Color.valueOf("#4fC3F7")
+    val light_blue_400: Color = Color.valueOf("#29B6FC")
+    val light_blue_500: Color = Color.valueOf("#03A9F4")
+    val light_blue_600: Color = Color.valueOf("#039BE5")
+    val light_blue_700: Color = Color.valueOf("#0288D1")
+    val light_blue_800: Color = Color.valueOf("#0277BD")
+    val light_blue_900: Color = Color.valueOf("#01579B")
+    val light_blue_A100: Color = Color.valueOf("#80D8FF")
+    val light_blue_A200: Color = Color.valueOf("#40C4FF")
+    val light_blue_A400: Color = Color.valueOf("#00B0FF")
+    val light_blue_A700: Color = Color.valueOf("#0091EA")
+
+    val light_blue_primary = light_blue_500
+    val light_blue_primary_light = light_blue_100
+    val light_blue_primary_dark = light_blue_700
+
+    val light_blue_accent = light_blue_A200
+    val light_blue_accent_light = light_blue_A100
+    val light_blue_accent_dark = light_blue_A400
+
+    /** cyan */
+    val cyan_50: Color = Color.valueOf("#E0F7FA")
+    val cyan_100: Color = Color.valueOf("#B2EBF2")
+    val cyan_200: Color = Color.valueOf("#80DEEA")
+    val cyan_300: Color = Color.valueOf("#4DD0E1")
+    val cyan_400: Color = Color.valueOf("#26C6DA")
+    val cyan_500: Color = Color.valueOf("#00BCD4")
+    val cyan_600: Color = Color.valueOf("#00ACC1")
+    val cyan_700: Color = Color.valueOf("#0097A7")
+    val cyan_800: Color = Color.valueOf("#00838F")
+    val cyan_900: Color = Color.valueOf("#006064")
+    val cyan_A100: Color = Color.valueOf("#84FFFF")
+    val cyan_A200: Color = Color.valueOf("#18FFFF")
+    val cyan_A400: Color = Color.valueOf("#00E5FF")
+    val cyan_A700: Color = Color.valueOf("#00B8D4")
+
+    val cyan_primary = cyan_500
+    val cyan_primary_light = cyan_100
+    val cyan_primary_dark = cyan_700
+
+    val cyan_accent = cyan_A200
+    val cyan_accent_light = cyan_A100
+    val cyan_accent_dark = cyan_A400
+
+    /** teal */
+    val teal_50: Color = Color.valueOf("#E0F2F1")
+    val teal_100: Color = Color.valueOf("#B2DFDB")
+    val teal_200: Color = Color.valueOf("#80CBC4")
+    val teal_300: Color = Color.valueOf("#4DB6AC")
+    val teal_400: Color = Color.valueOf("#26A69A")
+    val teal_500: Color = Color.valueOf("#009688")
+    val teal_600: Color = Color.valueOf("#00897B")
+    val teal_700: Color = Color.valueOf("#00796B")
+    val teal_800: Color = Color.valueOf("#00695C")
+    val teal_900: Color = Color.valueOf("#004D40")
+    val teal_A100: Color = Color.valueOf("#A7FFEB")
+    val teal_A200: Color = Color.valueOf("#64FFDA")
+    val teal_A400: Color = Color.valueOf("#1DE9B6")
+    val teal_A700: Color = Color.valueOf("#00BFA5")
+
+    val teal_primary = teal_500
+    val teal_primary_light = teal_100
+    val teal_primary_dark = teal_700
+
+    val teal_accent = teal_A200
+    val teal_accent_light = teal_A100
+    val teal_accent_dark = teal_A400
+
+    /** green */
+    val green_50: Color = Color.valueOf("#E8F5E9")
+    val green_100: Color = Color.valueOf("#C8E6C9")
+    val green_200: Color = Color.valueOf("#A5D6A7")
+    val green_300: Color = Color.valueOf("#81C784")
+    val green_400: Color = Color.valueOf("#66BB6A")
+    val green_500: Color = Color.valueOf("#4CAF50")
+    val green_600: Color = Color.valueOf("#43A047")
+    val green_700: Color = Color.valueOf("#388E3C")
+    val green_800: Color = Color.valueOf("#2E7D32")
+    val green_900: Color = Color.valueOf("#1B5E20")
+    val green_A100: Color = Color.valueOf("#B9F6CA")
+    val green_A200: Color = Color.valueOf("#69F0AE")
+    val green_A400: Color = Color.valueOf("#00E676")
+    val green_A700: Color = Color.valueOf("#00C853")
+
+    val green_primary = green_500
+    val green_primary_light = green_100
+    val green_primary_dark = green_700
+
+    val green_accent = green_A200
+    val green_accent_light = green_A100
+    val green_accent_dark = green_A400
+
+    /**light green*/
+    val light_green_50: Color = Color.valueOf("#F1F8E9")
+    val light_green_100: Color = Color.valueOf("#DCEDC8")
+    val light_green_200: Color = Color.valueOf("#C5E1A5")
+    val light_green_300: Color = Color.valueOf("#AED581")
+    val light_green_400: Color = Color.valueOf("#9CCC65")
+    val light_green_500: Color = Color.valueOf("#8BC34A")
+    val light_green_600: Color = Color.valueOf("#7CB342")
+    val light_green_700: Color = Color.valueOf("#689F38")
+    val light_green_800: Color = Color.valueOf("#558B2F")
+    val light_green_900: Color = Color.valueOf("#33691E")
+    val light_green_A100: Color = Color.valueOf("#CCFF90")
+    val light_green_A200: Color = Color.valueOf("#B2FF59")
+    val light_green_A400: Color = Color.valueOf("#76FF03")
+    val light_green_A700: Color = Color.valueOf("#64DD17")
+
+    val light_green_primary = light_green_500
+    val light_green_primary_light = light_green_100
+    val light_green_primary_dark = light_green_700
+
+    val light_green_accent = light_green_A200
+    val light_green_accent_light = light_green_A100
+    val light_green_accent_dark = light_green_A400
+
+    /** lime*/
+    val lime_50: Color = Color.valueOf("#F9FBE7")
+    val lime_100: Color = Color.valueOf("#F0F4C3")
+    val lime_200: Color = Color.valueOf("#E6EE9C")
+    val lime_300: Color = Color.valueOf("#DCE775")
+    val lime_400: Color = Color.valueOf("#D4E157")
+    val lime_500: Color = Color.valueOf("#CDDC39")
+    val lime_600: Color = Color.valueOf("#C0CA33")
+    val lime_700: Color = Color.valueOf("#A4B42B")
+    val lime_800: Color = Color.valueOf("#9E9D24")
+    val lime_900: Color = Color.valueOf("#827717")
+    val lime_A100: Color = Color.valueOf("#F4FF81")
+    val lime_A200: Color = Color.valueOf("#EEFF41")
+    val lime_A400: Color = Color.valueOf("#C6FF00")
+    val lime_A700: Color = Color.valueOf("#AEEA00")
+
+    val lime_primary = lime_500
+    val lime_primary_light = lime_100
+    val lime_primary_dark = lime_700
+
+    val lime_accent = lime_A200
+    val lime_accent_light = lime_A100
+    val lime_accent_dark = lime_A400
+
+    /**yellow */
+    val yellow_50: Color = Color.valueOf("#FFFDE7")
+    val yellow_100: Color = Color.valueOf("#FFF9C4")
+    val yellow_200: Color = Color.valueOf("#FFF590")
+    val yellow_300: Color = Color.valueOf("#FFF176")
+    val yellow_400: Color = Color.valueOf("#FFEE58")
+    val yellow_500: Color = Color.valueOf("#FFEB3B")
+    val yellow_600: Color = Color.valueOf("#FDD835")
+    val yellow_700: Color = Color.valueOf("#FBC02D")
+    val yellow_800: Color = Color.valueOf("#F9A825")
+    val yellow_900: Color = Color.valueOf("#F57F17")
+    val yellow_A100: Color = Color.valueOf("#FFFF82")
+    val yellow_A200: Color = Color.valueOf("#FFFF00")
+    val yellow_A400: Color = Color.valueOf("#FFEA00")
+    val yellow_A700: Color = Color.valueOf("#FFD600")
+
+    val yellow_primary = yellow_500
+    val yellow_primary_light = yellow_100
+    val yellow_primary_dark = yellow_700
+
+    val yellow_accent = yellow_A200
+    val yellow_accent_light = yellow_A100
+    val yellow_accent_dark = yellow_A400
+
+    /**amber*/
+    val amber_50: Color = Color.valueOf("#FFF8E1")
+    val amber_100: Color = Color.valueOf("#FFECB3")
+    val amber_200: Color = Color.valueOf("#FFE082")
+    val amber_300: Color = Color.valueOf("#FFD54F")
+    val amber_400: Color = Color.valueOf("#FFCA28")
+    val amber_500: Color = Color.valueOf("#FFC107")
+    val amber_600: Color = Color.valueOf("#FFB300")
+    val amber_700: Color = Color.valueOf("#FFA000")
+    val amber_800: Color = Color.valueOf("#FF8F00")
+    val amber_900: Color = Color.valueOf("#FF6F00")
+    val amber_A100: Color = Color.valueOf("#FFE57F")
+    val amber_A200: Color = Color.valueOf("#FFD740")
+    val amber_A400: Color = Color.valueOf("#FFC400")
+    val amber_A700: Color = Color.valueOf("#FFAB00")
+
+    val amber_primary = amber_500
+    val amber_primary_light = amber_100
+    val amber_primary_dark = amber_700
+
+    val amber_accent = amber_A200
+    val amber_accent_light = amber_A100
+    val amber_accent_dark = amber_A400
+
+    /**orange*/
+    val orange_50: Color = Color.valueOf("#FFF3E0")
+    val orange_100: Color = Color.valueOf("#FFE0B2")
+    val orange_200: Color = Color.valueOf("#FFCC80")
+    val orange_300: Color = Color.valueOf("#FFB74D")
+    val orange_400: Color = Color.valueOf("#FFA726")
+    val orange_500: Color = Color.valueOf("#FF9800")
+    val orange_600: Color = Color.valueOf("#FB8C00")
+    val orange_700: Color = Color.valueOf("#F57C00")
+    val orange_800: Color = Color.valueOf("#EF6C00")
+    val orange_900: Color = Color.valueOf("#E65100")
+    val orange_A100: Color = Color.valueOf("#FFD180")
+    val orange_A200: Color = Color.valueOf("#FFAB40")
+    val orange_A400: Color = Color.valueOf("#FF9100")
+    val orange_A700: Color = Color.valueOf("#FF6D00")
+
+    val orange_primary = orange_500
+    val orange_primary_light = orange_100
+    val orange_primary_dark = orange_700
+
+    val orange_accent = orange_A200
+    val orange_accent_light = orange_A100
+    val orange_accent_dark = orange_A400
+
+    /**deep orange*/
+    val deep_orange_50: Color = Color.valueOf("#FBE9A7")
+    val deep_orange_100: Color = Color.valueOf("#FFCCBC")
+    val deep_orange_200: Color = Color.valueOf("#FFAB91")
+    val deep_orange_300: Color = Color.valueOf("#FF8A65")
+    val deep_orange_400: Color = Color.valueOf("#FF7043")
+    val deep_orange_500: Color = Color.valueOf("#FF5722")
+    val deep_orange_600: Color = Color.valueOf("#F4511E")
+    val deep_orange_700: Color = Color.valueOf("#E64A19")
+    val deep_orange_800: Color = Color.valueOf("#D84315")
+    val deep_orange_900: Color = Color.valueOf("#BF360C")
+    val deep_orange_A100: Color = Color.valueOf("#FF9E80")
+    val deep_orange_A200: Color = Color.valueOf("#FF6E40")
+    val deep_orange_A400: Color = Color.valueOf("#FF3D00")
+    val deep_orange_A700: Color = Color.valueOf("#DD2600")
+
+    val deep_orange_primary = deep_orange_500
+    val deep_orange_primary_light = deep_orange_100
+    val deep_orange_primary_dark = deep_orange_700
+
+    val deep_orange_accent = deep_orange_A200
+    val deep_orange_accent_light = deep_orange_A100
+    val deep_orange_accent_dark = deep_orange_A400
+
+    /**brown */
+    val brown_50: Color = Color.valueOf("#EFEBE9")
+    val brown_100: Color = Color.valueOf("#D7CCC8")
+    val brown_200: Color = Color.valueOf("#BCAAA4")
+    val brown_300: Color = Color.valueOf("#A1887F")
+    val brown_400: Color = Color.valueOf("#8D6E63")
+    val brown_500: Color = Color.valueOf("#795548")
+    val brown_600: Color = Color.valueOf("#6D4C41")
+    val brown_700: Color = Color.valueOf("#5D4037")
+    val brown_800: Color = Color.valueOf("#4E342E")
+    val brown_900: Color = Color.valueOf("#3E2723")
+
+    val brown_primary = brown_500
+    val brown_primary_light = brown_100
+    val brown_primary_dark = brown_700
+
+    /**grey*/
+    val grey_50: Color = Color.valueOf("#FAFAFA")
+    val grey_100: Color = Color.valueOf("#F5F5F5")
+    val grey_200: Color = Color.valueOf("#EEEEEE")
+    val grey_300: Color = Color.valueOf("#E0E0E0")
+    val grey_400: Color = Color.valueOf("#BDBDBD")
+    val grey_500: Color = Color.valueOf("#9E9E9E")
+    val grey_600: Color = Color.valueOf("#757575")
+    val grey_700: Color = Color.valueOf("#616161")
+    val grey_800: Color = Color.valueOf("#424242")
+    val grey_900: Color = Color.valueOf("#212121")
+    val black_1000: Color = Color.valueOf("#000000")
+    val white_1000: Color = Color.valueOf("#ffffff")
+
+    val grey_primary = grey_500
+    val grey_primary_light = grey_100
+    val grey_primary_dark = grey_700
+
+    /**blue grey*/
+    val blue_grey_50: Color = Color.valueOf("#ECEFF1")
+    val blue_grey_100: Color = Color.valueOf("#CFD8DC")
+    val blue_grey_200: Color = Color.valueOf("#B0BBC5")
+    val blue_grey_300: Color = Color.valueOf("#90A4AE")
+    val blue_grey_400: Color = Color.valueOf("#78909C")
+    val blue_grey_500: Color = Color.valueOf("#607D8B")
+    val blue_grey_600: Color = Color.valueOf("#546E7A")
+    val blue_grey_700: Color = Color.valueOf("#455A64")
+    val blue_grey_800: Color = Color.valueOf("#37474F")
+    val blue_grey_900: Color = Color.valueOf("#263238")
+
+    val blue_grey_primary = blue_grey_500
+    val blue_grey_primary_light = blue_grey_100
+    val blue_grey_primary_dark = blue_grey_700
+
+}
